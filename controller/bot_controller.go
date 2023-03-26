61ab6feb02349a3f183d34e3c7d71f46d61699e3
SakagamiKazuto <47532989+SakagamiKazuto@users.noreply.github.com>
2023-03-26 22:23:16 +0900

    modify handleAppMention

--- controller/bot_controller.go
+++ controller/bot_controller.go
@@ -2,17 +2,12 @@ package controller
 
 import (
 	"bytes"
-	"encoding/json"
 	"feed-summary-bot/logger"
-	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/slack-go/slack"
 	"go.uber.org/zap"
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/slack-go/slack"
-	"github.com/slack-go/slack/slackevents"
 )
 
 func verifyRequest(c echo.Context, body string) error {
@@ -51,53 +46,22 @@ func HandleBotEvents(c echo.Context) error {
 		logger.LOG.Error("SlashParse failed", zap.Error(err))
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid slash command"})
 	}
+	handleAppMention(cmd)
 
-	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
-	if err != nil {
-		logger.LOG.Error("ParseEvent error", zap.Error(err), zap.String("body", body))
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-
-	switch eventsAPIEvent.Type {
-	case slackevents.CallbackEvent:
-		switch eventsAPIEvent.Type {
-		case slackevents.CallbackEvent:
-			innerEvent := eventsAPIEvent.InnerEvent
-			switch innerEventData := innerEvent.Data.(type) {
-			case *slackevents.AppMentionEvent:
-				handleAppMention(innerEventData, cmd)
-			default:
-				logger.LOG.Error(fmt.Sprintf("[INFO] unsupported inner event: %+v\n", innerEvent.Data))
-			}
-		default:
-			logger.LOG.Error(fmt.Sprintf("[INFO] unsupported event"))
-		}
-	}
-
-	return c.String(http.StatusOK, "")
+	return c.String(http.StatusOK, "successful posted")
 }
 
-func handleAppMention(event *slackevents.AppMentionEvent, cmd slack.SlashCommand) {
+func handleAppMention(cmd slack.SlashCommand) {
 	api := slack.New(os.Getenv("SLACK_APP_TOKEN"))
 
-	command := strings.Split(event.Text, " ")
-
-	if len(command) < 3 {
-		logger.LOG.Error("command < 3")
-		api.PostMessage(event.Channel, slack.MsgOptionText("Error: Invalid command format.", false))
-		return
-	}
-
-	action, url := command[1], command[2]
-
-	switch action {
-	case "feed":
+	switch cmd.Command {
+	case "/summary-feed":
 		// ここに保存されたchannelIDとfeedURLを保存する処理を追加
 		// channelIDはcmdから、feedURLはurl変数から取得する。
-		response := "Feed URL '" + url + "' has been saved."
-		api.PostMessage(event.Channel, slack.MsgOptionText(response, false))
+		response := "Feed URL '" + cmd.Text + "' has been saved."
+		api.PostMessage(cmd.ChannelID, slack.MsgOptionText(response, false))
 	default:
 		logger.LOG.Error("invalid command format")
-		api.PostMessage(event.Channel, slack.MsgOptionText("Error: Invalid command format.", false))
+		api.PostMessage(cmd.ChannelID, slack.MsgOptionText("Error: Invalid command format.", false))
 	}
 }
